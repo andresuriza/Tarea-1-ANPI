8e324d298608a390d299d404c9d0e758eba53b0d
Felipe Vargas <68176209+Felipe-V13@users.noreply.github.com>
2024-02-25 23:22:32 -0600

    Add files via upload
    and cos_t and ln_t

--- anpi2/anpi2/funtras.cpp
+++ anpi2/anpi2/funtras.cpp
@@ -1,4 +1,4 @@
-#include "funtras.h"
+﻿#include "funtras.h"
 #include <iostream>
 #include <boost/multiprecision/cpp_dec_float.hpp>
 
@@ -107,11 +107,96 @@ using namespace boost::multiprecision;
         }
     }
 
+
+
+
+    //cos
+
+
+    cpp_dec_float_50 Fun_tras::cos_t(cpp_dec_float_50 a) {
+ 
+        cpp_dec_float_50 sum = 1; // El primer término de la serie de Taylor para cos(a) es 1.
+        cpp_dec_float_50 term = 1; // Inicializamos el término actual de la serie.
+        cpp_dec_float_50 prev_sum = 0; // Variable para almacenar la suma anterior y comprobar la convergencia.
+
+        // Iterar hasta que la serie converja o se alcance el número máximo de iteraciones.
+        for (int n = 1; n <= iterMax && abs(term) >= tol; ++n) {
+            term *= -a * a / ((2 * n - 1) * (2 * n)); // Actualizar el término actual de la serie.
+            prev_sum = sum; // Guardar la suma anterior antes de añadir el nuevo término.
+            sum += term; // Añadir el nuevo término a la suma acumulativa.
+
+
+            if (abs(sum - prev_sum) < tol) {
+                break;
+            }
+        }
+
+        return sum;
+    }
+
+
+
+
+    cpp_dec_float_50 Fun_tras::abs_t(const cpp_dec_float_50& x) {
+        if (x < 0)
+            return -1 * x;
+        else
+            return x;
+    }
+
+
+    cpp_dec_float_50 Fun_tras::ln_t(cpp_dec_float_50 a) {
+        if (a <= 0) {
+            cout << "Error: 'a' debe ser mayor que 0" << endl;
+            return cpp_dec_float_50(0); // Retornar 0 para indicar un error.
+        }
+
+        cpp_dec_float_50 factor = 2 * (a - 1) / (a + 1);
+
+        cpp_dec_float_50 x = (a - 1) / (a + 1);
+        cpp_dec_float_50 x_pow_2n = x; // Inicia con x^1 ya que n=0.
+
+        cpp_dec_float_50 sum = x; // Inicializar con el primer término de la serie.
+
+        cpp_dec_float_50 prev_sum = 0;
+
+        for (int n = 1; n <= iterMax; ++n) {
+            x_pow_2n *= x * x;
+
+         
+            cpp_dec_float_50 current_term = x_pow_2n / (2 * n + 1);
+
+            prev_sum = sum;
+            sum += current_term;
+
+            if (abs(current_term) < tol) {
+                break;
+            }
+        }
+
+        return factor * sum;
+    }
+
+
+
+
 int main(int argc, char const* argv[])
 {
     Fun_tras calc1;
     cpp_dec_float_50 result = calc1.sinh_t(3); // Caso de prueba
 
     cout << setprecision(std::numeric_limits<cpp_dec_float_50>::max_digits10) << result << endl;
+
+
+    // Prueba para la función cos_t con π/3 como argumento
+    cpp_dec_float_50 cos_result = calc1.cos_t(4); // cos(4)
+    cout  << setprecision(std::numeric_limits<cpp_dec_float_50>::max_digits10) << cos_result << endl;
+
+    // Prueba para la función ln_t con e (base de los logaritmos naturales) como argumento
+    cpp_dec_float_50 ln_result = calc1.ln_t(6); // ln(e)
+    cout << setprecision(std::numeric_limits<cpp_dec_float_50>::max_digits10) << ln_result << endl;
+
+
+
     return 0;
 }
--- anpi2/anpi2/funtras.h
+++ anpi2/anpi2/funtras.h
@@ -12,6 +12,7 @@ private:
     int iterMax = 2500;
     cpp_dec_float_50 pi_t = 3.14159265358979323846;
     cpp_dec_float_50 eps = 2.2204 * pow(10, -16);
+  
 
 public:
 
@@ -19,5 +20,10 @@ public:
     cpp_dec_float_50 divi_t(cpp_dec_float_50 x);
     cpp_dec_float_50 sin_t(int x);
     cpp_dec_float_50 sinh_t(int x);
+    cpp_dec_float_50 cos_t(cpp_dec_float_50 x);
+    cpp_dec_float_50 ln_t(cpp_dec_float_50 x);
+    cpp_dec_float_50 abs_t(cpp_dec_float_50 x);
+
 };
+
 
