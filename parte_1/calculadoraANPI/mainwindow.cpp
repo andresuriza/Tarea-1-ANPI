1b6438ab9a358dd67a5a9a154975953561336625
Ignacio Calderón <53590021+Apollo13-0@users.noreply.github.com>
2024-03-02 20:57:42 -0600

    empty slots

--- parte_1/calculadoraANPI/mainwindow.cpp
+++ parte_1/calculadoraANPI/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <iostream>
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +13,211 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_helpButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+
+}
+
+void MainWindow::on_senhButton_clicked()
+{
+
+}
+void MainWindow::on_tanhButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_coshButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_asenButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_acosButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_atanButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_secButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_cscButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_cotButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_senButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_cosButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_tanButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_lnButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_logTenButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_logYButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_divButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_sqrButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_sqrYButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_expButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_powerYButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_factorialButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_sevenButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_eightButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_nineButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_fourButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_fiveButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_sixButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_oneButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_twoButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_threeButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_piButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_zeroButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_23_clicked()
+{
+
 }
 
--- parte_1/calculadoraANPI/mainwindow.h
+++ parte_1/calculadoraANPI/mainwindow.h
@@ -15,6 +15,77 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_senhButton_clicked();
+
+    void on_helpButton_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_tanhButton_clicked();
+
+    void on_coshButton_clicked();
+
+    void on_asenButton_clicked();
+
+    void on_acosButton_clicked();
+
+    void on_atanButton_clicked();
+
+    void on_secButton_clicked();
+
+    void on_cscButton_clicked();
+
+    void on_cotButton_clicked();
+
+    void on_senButton_clicked();
+
+    void on_cosButton_clicked();
+
+    void on_tanButton_clicked();
+
+    void on_lnButton_clicked();
+
+    void on_logTenButton_clicked();
+
+    void on_logYButton_clicked();
+
+    void on_divButton_clicked();
+
+    void on_sqrButton_clicked();
+
+    void on_sqrYButton_clicked();
+
+    void on_expButton_clicked();
+
+    void on_powerYButton_clicked();
+
+    void on_factorialButton_clicked();
+
+    void on_sevenButton_clicked();
+
+    void on_eightButton_clicked();
+
+    void on_nineButton_clicked();
+
+    void on_fourButton_clicked();
+
+    void on_fiveButton_clicked();
+
+    void on_sixButton_clicked();
+
+    void on_oneButton_clicked();
+
+    void on_twoButton_clicked();
+
+    void on_threeButton_clicked();
+
+    void on_piButton_clicked();
+
+    void on_zeroButton_clicked();
+
+    void on_pushButton_23_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
